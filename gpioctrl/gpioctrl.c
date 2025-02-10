7ad9ac53eea1b8ef3e0831d7a04fdc014035a770
Klaudiusz Pastor <pastor.klo12@gmail.com>
2025-02-10 19:53:25 +0100

    gpio

---
+++ gpioctrl/gpioctrl.c
@@ -0,0 +1,55 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/gpio/consumer.h>
+
+static struct gpio_desc *led, *button;
+
+#define IO_LED 21
+#define IO_BUTTON 20
+#define IO_OFFSET 0
+
+static int my_init(void) 
+{
+	int status;
+
+	led = gpio_to_desc(IO_LED);	
+	if (!led) {
+		printk("gpioctrl - Error getting pin 21\n");
+		return -ENODEV;
+	}
+
+	button = gpio_to_desc(IO_BUTTON);	
+	if (!button) {
+		printk("gpioctrl - Error getting pin 20\n");
+		return -ENODEV;
+	}
+
+	status = gpiod_direction_output(led,0);
+	if (status) {
+		printk("gpioctrl - Error getting pin 21 to output\n");
+		return status;	
+	}
+
+	status = gpiod_direction_input(button);
+	if (status) {
+		printk("gpioctrl - Error getting pin 20 to input\n");
+		return status;	
+	}
+
+	gpiod_set_value(led, 1);
+	printk("gpioctrl-Button is %s pressed \n", gpiod_get_value(button) ? "" : "not ");
+
+	return 0;
+}
+
+static void my_exit(void)
+{
+	gpiod_set_value(led, 0);
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("claudius");
+MODULE_DESCRIPTION("An example for using GPIOs");
